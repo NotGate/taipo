14ad62ac10b4fb8f6b9c0da5b4f794f6fe478497
Michael Millwood <imanotgate@gmail.com>
2020-04-08 22:28:07 -0500

    Reverting some changes for now. Going to work on something other than scenes for a bit.

--- src/game.rs
+++ src/game.rs
@@ -31,26 +31,6 @@ pub struct Game {
     font: graphics::Font,
     fps_text: graphics::Text,
     // Scene Stack
-    scene: String,
-    playing_scene: Scene,
-    main_scene: Scene,
-}
-
-struct Main {}
-struct Settings {}
-struct Help {}
-struct Select {}
-struct Playing {}
-struct Score {}
-
-enum Scene {
-    None,
-    Main(Main),
-    Settings(Settings),
-    Help(Help),
-    Select(Select),
-    Playing(Playing),
-    Score(Score)
 }
 
 impl Game {
@@ -78,11 +58,6 @@ impl Game {
         mp.set_volume(0.6)?;
         mp.play()?;
 
-        // SceneStack
-        let main_scene = Scene::Main(Main {});
-        let playing_scene = Scene::Playing(Playing {});
-        let scene =String::from("Main");
-
         // Resources (TODO:where do I store all these?)
         let font = graphics::Font::new(&mut ctx, "/fonts/consola.ttf").map_err(|e| format!("Could not find font: {}", e))?;
         let fps_text = graphics::Text::new((ggez::timer::fps(&mut ctx).to_string(), font, 48.0));
@@ -96,9 +71,6 @@ impl Game {
             osu_p,
             font,
             fps_text,
-            scene,
-            main_scene,
-            playing_scene
         })
     }
     pub fn tick(&mut self) -> Result<(), String> {
@@ -107,50 +79,13 @@ impl Game {
         Ok(())
     }
     pub fn poll(&mut self) -> Result<(), String> {
-
-        // Systems iterating over objects instead of giving my Scene their own functions
-        // working with their data
-        // I am implementing scenes is just using enumerators and match statement. Different scenes will run different systems and will work with different components
-
         for (e, s, k, m) in process(&mut self.el) {
-            match self.scene.as_str() {
-                "Main" => {
-                    f(self);
-                }
-                "Playing" => {
-                    g(self);
-                },
-                _ => (),
-            }
-            // self.ctx.process_event(&e);
-            // use std::borrow::BorrowMut;
-            // use std::boxed::*;
-            // use std::any::Any;
-            // // let thing:Box<dyn Scene> = self.ss[0].downcast::<MainScene>();
-            // let thing:Box<dyn Any> = self.ss[0];
-            // if let Ok(string) = thing.downcast::<MainScene>() {
-            //     println!("String ({}): {}", string.len(), string);
-            // }
-            // match self.scene.as_str() {
-            //     "Playing" => self.ss[0].poll(self, &e,&s,&k,&m),
-            //     _ => self.scene = "Main".into()
-            // }
-            // self.ss[0].poll(&mut self.db, &e,&s,&k,&m);
         }
         Ok(())
     }
     pub fn update(&mut self) -> Result<(), String> {
         // TODO: mp.pos should check if playing
         // println!("{}", self.mp.pos()?);
-        match self.scene.as_str() {
-            "Main" => {
-                f(self);
-            }
-            "Playing" => {
-                g(self);
-            },
-            _ => (),
-        }
         Ok(())
     }
     pub fn render(&mut self) -> Result<(), String> {
@@ -178,14 +113,4 @@ pub fn process(el: &mut EventsLoop) -> Vec<(Event, ElementState, KeyCode, Modifi
         _ => (),
     });
     events
-}
-
-pub fn f(g:&mut Game) {
-    println!("Playing");
-    g.scene = String::from("Playing");
-}
-
-pub fn g(g:&mut Game) {
-    println!("Main");
-    g.scene = String::from("Main");
 }
